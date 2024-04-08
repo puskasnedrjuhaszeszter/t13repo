a947abc9e8c53fa51885bc02ed697519c7edca94
puskasnedrjuhaszeszter <152326224+puskasnedrjuhaszeszter@users.noreply.github.com>
2024-04-08 21:27:03 +0200

    UnitTeszt gyakorló

---
+++ SZ01_UnitTesztKészítés/bootstrapRegisztracioTeszt.js
@@ -0,0 +1,582 @@
+function IgazitoDivClassEllenorzes() {
+    let tesztElem = document.querySelector("div");
+    if (tesztElem.classList.contains("container")) {
+        console.log("Sikeres 01. teszt");
+    }
+    else {
+        console.log("Sikertelen 01. teszt");
+    }
+}
+IgazitoDivClassEllenorzes();
+
+function CimsorTartalomTeszt() {
+    let tesztElem = document.querySelector("h1").textContent;
+    if (tesztElem == "Tanfolyam regisztráció") {
+        console.log("Sikeres 02. teszt");
+    }
+    else {
+        console.log("Sikertelen 02. teszt")
+    }
+}
+CimsorTartalomTeszt();
+
+
+function InputMezoTipusEllenorzes01() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[0].type == "text") {
+        console.log("Sikeres 03. teszt");
+    }
+    else {
+        console.log("Sikertelen 03. teszt")
+    }
+}
+InputMezoTipusEllenorzes01();
+
+function InputMezoIdEllenorzes01() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[0].id == "veznev") {
+        console.log("Sikeres 04. teszt");
+    }
+    else {
+        console.log("Sikertelen 04. teszt")
+    }
+}
+InputMezoIdEllenorzes01();
+
+function InputMezoFormEllenorzes01() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[0].classList == "form-control") {
+        console.log("Sikeres 05. teszt");
+    }
+    else {
+        console.log("Sikertelen 05. teszt")
+    }
+}
+InputMezoFormEllenorzes01();
+
+function InputMezoTipusEllenorzes02() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[1].type == "text") {
+        console.log("Sikeres 06. teszt");
+    }
+    else {
+        console.log("Sikertelen 06. teszt")
+    }
+}
+InputMezoTipusEllenorzes02();
+
+function InputMezoIdEllenorzes02() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[1].id == "kernev") {
+        console.log("Sikeres 07. teszt");
+    }
+    else {
+        console.log("Sikertelen 07. teszt")
+    }
+}
+InputMezoIdEllenorzes02();
+
+function InputMezoFormEllenorzes02() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[1].classList == "form-control") {
+        console.log("Sikeres 08. teszt");
+    }
+    else {
+        console.log("Sikertelen 08. teszt")
+    }
+}
+InputMezoFormEllenorzes02();
+
+function InputMezoTipusEllenorzes03() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[2].type == "text") {
+        console.log("Sikeres 09. teszt");
+    }
+    else {
+        console.log("Sikertelen 09. teszt")
+    }
+}
+InputMezoTipusEllenorzes03();
+
+function InputMezoIdEllenorzes03() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[2].id == "fnev") {
+        console.log("Sikeres 10. teszt");
+    }
+    else {
+        console.log("Sikertelen 10. teszt")
+    }
+}
+InputMezoIdEllenorzes03();
+
+function InputMezoFormEllenorzes03() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[2].classList == "form-control") {
+        console.log("Sikeres 11. teszt");
+    }
+    else {
+        console.log("Sikertelen 11. teszt")
+    }
+}
+InputMezoFormEllenorzes03();
+
+function InputMezoTipusEllenorzes04() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[3].type == "password") {
+        console.log("Sikeres 12. teszt");
+    }
+    else {
+        console.log("Sikertelen 12. teszt")
+    }
+}
+InputMezoTipusEllenorzes04();
+
+function InputMezoIdEllenorzes04() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[3].id == "pass1") {
+        console.log("Sikeres 13. teszt");
+    }
+    else {
+        console.log("Sikertelen 13. teszt")
+    }
+}
+InputMezoIdEllenorzes04();
+
+function InputMezoFormEllenorzes04() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[3].classList == "form-control") {
+        console.log("Sikeres 14. teszt");
+    }
+    else {
+        console.log("Sikertelen 14. teszt")
+    }
+}
+InputMezoFormEllenorzes04();
+
+function InputMezoTipusEllenorzes05() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[4].type == "password") {
+        console.log("Sikeres 15. teszt");
+    }
+    else {
+        console.log("Sikertelen 15. teszt")
+    }
+}
+InputMezoTipusEllenorzes05();
+
+function InputMezoIdEllenorzes05() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[4].id == "pass2") {
+        console.log("Sikeres 16. teszt");
+    }
+    else {
+        console.log("Sikertelen 16. teszt")
+    }
+}
+InputMezoIdEllenorzes05();
+
+function InputMezoFormEllenorzes05() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[4].classList == "form-control") {
+        console.log("Sikeres 17. teszt");
+    }
+    else {
+        console.log("Sikertelen 17. teszt")
+    }
+}
+InputMezoFormEllenorzes05();
+
+function InputMezoIdEllenorzes06() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[5].id == "email") {
+        console.log("Sikeres 18. teszt");
+    }
+    else {
+        console.log("Sikertelen 18. teszt")
+    }
+}
+InputMezoIdEllenorzes06();
+
+function InputMezoTipusEllenorzes06() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[5].type == "email") {
+        console.log("Sikeres 19. teszt");
+    }
+    else {
+        console.log("Sikertelen 19. teszt")
+    }
+}
+InputMezoTipusEllenorzes06();
+
+function InputMezoFormEllenorzes06() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[5].classList == "form-control") {
+        console.log("Sikeres 20. teszt");
+    }
+    else {
+        console.log("Sikertelen 20. teszt")
+    }
+}
+InputMezoFormEllenorzes06();
+
+function InputMezoTipusEllenorzes07() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[6].type == "tel") {
+        console.log("Sikeres 21. teszt");
+    }
+    else {
+        console.log("Sikertelen 21. teszt")
+    }
+}
+InputMezoTipusEllenorzes07();
+
+function InputMezoIdEllenorzes07() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[6].id == "tel") {
+        console.log("Sikeres 22. teszt");
+    }
+    else {
+        console.log("Sikertelen 22. teszt")
+    }
+}
+InputMezoIdEllenorzes07();
+
+function InputMezoFormEllenorzes07() {
+    let tesztElem = document.querySelectorAll("input");
+    if (tesztElem[6].classList == "form-control") {
+        console.log("Sikeres 23. teszt");
+    }
+    else {
+        console.log("Sikertelen 23. teszt")
+    }
+}
+InputMezoFormEllenorzes07();
+
+function FormGroupEllenorzes01() {
+    let tesztElem = document.querySelectorAll("div");
+    if (tesztElem[3].classList == "form-group") {
+        console.log("Sikeres 24/A. teszt");
+    }
+    else {
+        console.log("Sikertelen 24/A. teszt")
+    }
+}
+FormGroupEllenorzes01();
+
+function FormGroupEllenorzes02() {
+    let tesztElem = document.querySelectorAll("div");
+    if (tesztElem[5].classList == "form-group") {
+        console.log("Sikeres 24/B. teszt");
+    }
+    else {
+        console.log("Sikertelen 24/B. teszt")
+    }
+}
+FormGroupEllenorzes02();
+
+function labelEllenorzes01() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[0].textContent == "Vezeték név:") {
+        console.log("Sikeres 25/A. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/A. teszt");
+    }
+}
+labelEllenorzes01();
+
+function labelEllenorzes01() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[0].textContent == "Vezeték név:") {
+        console.log("Sikeres 25/A. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/A. teszt");
+    }
+}
+labelEllenorzes01();
+
+function labelEllenorzes02() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[1].textContent == "Kereszt név:") {
+        console.log("Sikeres 25/B. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/B. teszt");
+    }
+}
+labelEllenorzes02();
+
+function labelEllenorzes03() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[2].textContent == "Felhasználói név:") {
+        console.log("Sikeres 25/C. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/C. teszt");
+    }
+}
+labelEllenorzes03();
+
+function labelEllenorzes04() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[3].textContent == "Jelszó:") {
+        console.log("Sikeres 25/D. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/D. teszt");
+    }
+}
+labelEllenorzes04();
+
+function labelEllenorzes05() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[4].textContent == "Jelszó ismét:") {
+        console.log("Sikeres 25/E. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/E teszt");
+    }
+}
+labelEllenorzes05();
+
+function labelEllenorzes06() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[5].textContent == "E-mail cím:") {
+        console.log("Sikeres 25/F. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/F. teszt");
+    }
+}
+labelEllenorzes06();
+
+function labelEllenorzes07() {
+    let tesztElem = document.querySelectorAll("label");
+    if (tesztElem[6].textContent == "Telefonszám:") {
+        console.log("Sikeres 25/G. teszt");
+    }
+    else {
+        console.log("Sikertelen 25/G. teszt");
+    }
+}
+labelEllenorzes07();
+
+function smallOsztalyEllenorzes01() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[0].classList.contains("form-text")) {
+        console.log("Sikeres 26/A. teszt");
+    }
+    else {
+        console.log("Sikertelen 26/A. teszt");
+    }
+}
+smallOsztalyEllenorzes01();
+
+function smallOsztalyEllenorzes02() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[0].classList.contains("text-muted")) {
+        console.log("Sikeres 26/B. teszt");
+    }
+    else {
+        console.log("Sikertelen 26/B. teszt");
+    }
+}
+smallOsztalyEllenorzes02();
+
+function smallTartalomEllenorzes01() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[0].textContent == "Beceneve, mely mások számára is látható.") {
+        console.log("Sikeres 27. teszt");
+    }
+    else {
+        console.log("Sikertelen 27. teszt");
+    }
+}
+smallTartalomEllenorzes01();
+
+function smallOsztalyEllenorzes03() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[1].classList.contains("form-text")) {
+        console.log("Sikeres 28/A. teszt");
+    }
+    else {
+        console.log("Sikertelen 28/A. teszt");
+    }
+}
+smallOsztalyEllenorzes03();
+
+function smallOsztalyEllenorzes04() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[1].classList.contains("text-muted")) {
+        console.log("Sikeres 28/B. teszt");
+    }
+    else {
+        console.log("Sikertelen 28/B. teszt");
+    }
+}
+smallOsztalyEllenorzes04();
+
+function smallTartalomEllenorzes02() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[1].textContent == "Ide továbbítjuk a legfontosabb tanfolyam információkat önnek." ) {
+        console.log("Sikeres 29. teszt");
+    }
+    else {
+        console.log("Sikertelen 29. teszt");
+    }
+}
+smallTartalomEllenorzes02();
+
+function smallOsztalyEllenorzes05() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[2].classList.contains("form-text")) {
+        console.log("Sikeres 30/A. teszt");
+    }
+    else {
+        console.log("Sikertelen 30/A. teszt");
+    }
+}
+smallOsztalyEllenorzes05();
+
+function smallOsztalyEllenorzes06() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[2].classList.contains("text-muted")) {
+        console.log("Sikeres 30/B. teszt");
+    }
+    else {
+        console.log("Sikertelen 30/B. teszt");
+    }
+}
+smallOsztalyEllenorzes06();
+
+function smallTartalomEllenorzes03() {
+    let tesztElem = document.querySelectorAll("small");
+    if (tesztElem[2].textContent == "Ha szeretne akár azonnal értesülni a fontosabb hírekről" ) {
+        console.log("Sikeres 31. teszt");
+    }
+    else {
+        console.log("Sikertelen 31. teszt");
+    }
+}
+smallTartalomEllenorzes03();
+
+function smallOsztalyEllenorzesAll() {
+    let tesztElem = document.querySelector("small");
+    if (tesztElem.classList == "form-text text-muted") {
+        console.log("Sikeres 32. teszt");
+    }
+    else {
+        console.log("Sikertelen 32. teszt");
+    }
+}
+smallOsztalyEllenorzesAll();
+
+function buttonClassEllenorzes() {
+    let tesztElem = document.querySelector("button");
+    if (tesztElem.classList.contains("btn")) {
+        console.log("Sikeres 33. teszt");
+    }
+    else {
+        console.log("Sikertelen 33. teszt");
+    }
+}
+buttonClassEllenorzes();
+
+function buttonClassSzinEllenorzes() {
+    let tesztElem = document.querySelector("button");
+    if (tesztElem.classList.contains("btn-primary")) {
+        console.log("Sikeres 34. teszt");
+    }
+    else {
+        console.log("Sikertelen 34. teszt");
+    }
+}
+buttonClassSzinEllenorzes();
+
+function buttonClassMeretEllenorzes() {
+    let tesztElem = document.querySelector("button");
+    if (tesztElem.classList.contains("w-100")) {
+        console.log("Sikeres 35. teszt");
+    }
+    else {
+        console.log("Sikertelen 35. teszt");
+    }
+}
+buttonClassMeretEllenorzes();
+
+function buttonFeliratEllenorzes() {
+    let tesztElem = document.querySelector("button");
+    if (tesztElem.textContent == "Regisztrálok" ) {
+        console.log("Sikeres 36. teszt");
+    }
+    else {
+        console.log("Sikertelen 36. teszt");
+    }
+}
+buttonFeliratEllenorzes();
+
+function selectSzamEllenorzes () {
+    let tesztElem = document.querySelector("select");
+    if (tesztElem.length == 4){
+        console.log("Sikeres 37. teszt");
+    }
+    else {
+        console.log("Sikertelen 37. teszt");
+    }
+}
+selectSzamEllenorzes ();
+
+function selectErtekEllenorzes01 () {
+    let tesztElem = document.querySelectorAll("select");
+    if (tesztElem[0].value == "Webfejlesztő"){
+        console.log("Sikeres 38/A. teszt");
+    }
+    else {
+        console.log("Sikertelen 38/A. teszt");
+    }
+}
+selectErtekEllenorzes01 ();
+
+function selectErtekEllenorzes02 () {
+    let tesztElem = document.querySelectorAll("option");
+    if (tesztElem[1].value == "Junior frontend fejlesztő"){
+        console.log("Sikeres 38/B. teszt");
+    }
+    else {
+        console.log("Sikertelen 38/B. teszt");
+    }
+}
+selectErtekEllenorzes02 ();
+
+function selectErtekEllenorzes03 () {
+    let tesztElem = document.querySelectorAll("option");
+    if (tesztElem[2].value == "MySQL intenzív tanfolyam"){
+        console.log("Sikeres 38/C. teszt");
+    }
+    else {
+        console.log("Sikertelen 38/C. teszt");
+    }
+}
+
+selectErtekEllenorzes03 ();
+
+
+function selectErtekEllenorzes04 () {
+    let tesztElem = document.querySelectorAll("option");
+    if (tesztElem[3].value == "C# alapismeretek"){
+        console.log("Sikeres 38/D. teszt");
+    }
+    else {
+        console.log("Sikertelen 38/D. teszt");
+    }
+}
+
+selectErtekEllenorzes04 ();
+
+
+
+
+
+
+
